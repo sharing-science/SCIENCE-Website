c76dac6f7f38257f92e81fbb1caab2eb5f1e34ae
Noe Horowitz <noehoro@gmail.com>
2021-07-07 12:18:21 -0400

    Create Context for user info and reducer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import LandingPage from "./views/LandingPage";
@@ -6,45 +6,50 @@ import RegisterPage from "./views/RegisterPage";
 import ContactUsPage from "./views/ContactUsPage";
 import UploadFilePage from "./views/UploadFilePage";
 import ProfilePage from "./views/ProfilePage";
-// import LoginPage from "./views/LoginPage";
-import SampleContract from "views/SampleContractPage";
-import Context from "./Context";
-import { useState } from "react/cjs/react.production.min";
+import LoginPage from "./views/LoginPage";
+import SampleContract from "./views/SampleContractPage";
+import Context from "./Helpers/Context";
+import { contextReducer } from "Helpers/reducers";
+import { getLocalStorageObject } from "Helpers/helperFunctions";
 
 const App = () => {
-  const [context, setContext] = useState({});
-
-  /* <Context.Provider value={{ context, setContext }}>
-
-</Context.Provider> */
+  const [contextValue, dispatchContextValue] = useReducer(contextReducer, {
+    loggedIn: getLocalStorageObject("loggedIn"),
+    web3: getLocalStorageObject("web3"),
+  });
 
   return (
-    <Router>
-      <Switch>
-        <Route
-          path="/register-page"
-          render={(props) => <RegisterPage {...props} />}
-        />
-        {/* <Route
-          path="/login-page"
-          render={(props) => <LoginPage {...props} />}
-        /> */}
-        <Route
-          path="/contact-us"
-          render={(props) => <ContactUsPage {...props} />}
-        />
-        <Route
-          path="/upload"
-          render={(props) => <UploadFilePage {...props} />}
-        />
-        <Route
-          path="/contract"
-          render={(props) => <SampleContract {...props} />}
-        />
-        <Route path="/profile" render={(props) => <ProfilePage {...props} />} />
-        <Route path="/" render={(props) => <LandingPage {...props} />} />
-      </Switch>
-    </Router>
+    <Context.Provider value={{ contextValue, dispatchContextValue }}>
+      <Router>
+        <Switch>
+          <Route
+            path="/register-page"
+            render={(props) => <RegisterPage {...props} />}
+          />
+          <Route
+            path="/login-page"
+            render={(props) => <LoginPage {...props} />}
+          />
+          <Route
+            path="/contact-us"
+            render={(props) => <ContactUsPage {...props} />}
+          />
+          <Route
+            path="/upload"
+            render={(props) => <UploadFilePage {...props} />}
+          />
+          <Route
+            path="/contract"
+            render={(props) => <SampleContract {...props} />}
+          />
+          <Route
+            path="/profile"
+            render={(props) => <ProfilePage {...props} />}
+          />
+          <Route path="/" render={(props) => <LandingPage {...props} />} />
+        </Switch>
+      </Router>
+    </Context.Provider>
   );
 };
 
---
+++ client/src/Helpers/reducers.js
@@ -0,0 +1,22 @@
+const contextReducer = (state, action) => {
+  switch (action.type) {
+    case "login":
+      localStorage.setItem("web3", JSON.stringify(action.payload));
+      return {
+        ...state,
+        loggedIn: true,
+        web3: action.payload,
+      };
+    case "logout":
+      localStorage.removeItem("web3");
+      return {
+        ...state,
+        loggedIn: false,
+        web3: null,
+      };
+    default:
+      throw new Error();
+  }
+};
+
+export { contextReducer };
