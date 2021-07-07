16e112dc1c2a634eceb89a4e6db12094ddf4aafa
Noe Horowitz <noehoro@gmail.com>
2021-07-07 16:00:47 -0400

    Update SampleContractPage.jsx

--- client/src/views/SampleContractPage.jsx
+++ client/src/views/SampleContractPage.jsx
@@ -1,76 +1,106 @@
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
-// javascript plugin used to create scrollbars on windows
-import PerfectScrollbar from "perfect-scrollbar";
 // reactstrap components
-import { Button, Container, Row, Col } from "reactstrap";
+import { Button, Container, Row } from "reactstrap";
 
 // core components
 import NavBar from "components/NavBar";
 import Footer from "components/Footer";
-
-let ps = null;
+import Context from "Helpers/Context";
+import getWeb3 from "Helpers/getWeb3";
+import Covid19usecase from "../contracts/Covid19usecase.json";
 
 const SampleContract = () => {
-  useEffect(() => {
-    if (navigator.platform.indexOf("Win") > -1) {
-      document.documentElement.className += " perfect-scrollbar-on";
-      document.documentElement.classList.remove("perfect-scrollbar-off");
-      let tables = document.querySelectorAll(".table-responsive");
-      for (let i = 0; i < tables.length; i++) {
-        ps = new PerfectScrollbar(tables[i]);
-      }
-    }
-    document.body.classList.toggle("profile-page");
-    // Specify how to clean up after this effect:
-    return function cleanup() {
-      if (navigator.platform.indexOf("Win") > -1) {
-        ps.destroy();
-        document.documentElement.className += " perfect-scrollbar-off";
-        document.documentElement.classList.remove("perfect-scrollbar-on");
-      }
-      document.body.classList.toggle("profile-page");
-    };
-  }, []);
+  const { contextValue } = useContext(Context);
 
-  const handleClick = () => {
-    console.log("hello");
+  const [web3, setWeb3] = useState();
+  const [contract, setContract] = useState(undefined);
+  const [clauseList, setClauseList] = useState([]);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const _web3 = await getWeb3();
+        const instance = new _web3.eth.Contract(
+          Covid19usecase.abi,
+          Covid19usecase.networks[contextValue.web3.networkId] &&
+            Covid19usecase.networks[contextValue.web3.networkId].address
+        );
+
+        setWeb3(_web3);
+        setContract(instance);
+      } catch (error) {
+        console.log("Error");
+      }
+    };
+    init();
+  }, [contextValue.web3.networkId]);
+
+  const getClauses = async () => {
+    const clauseCount = await contract.methods.getClauseCount().call();
+    const _clauseList = [];
+    for (let i = 0; i < clauseCount; ++i) {
+      const clause = await contract.methods.getClause(i + 1).call();
+      _clauseList.push(clause);
+    }
+    setClauseList(_clauseList);
   };
+
+  const acceptClause = async (e) => {
+    await contract.methods
+      .acceptClause(e.target.name)
+      .send({ from: contextValue.web3.accounts[0] });
+  };
+
+  const checkAccepted = async () => {
+    for (let i = 0; i < clauseList.length; ++i) {
+      const status = await contract.methods.getClauseStatus(i).call();
+      console.log(`The Status of Clause number ${i} is ` + status);
+    }
+  };
+
   return (
     <>
       <NavBar />
-      <div className="wrapper">
+      <div className="wrapper register-page">
         <div className="page-header">
-          <img
-            alt="..."
-            className="dots"
-            src={require("assets/img/dots.png").default}
-          />
-          <img
-            alt="..."
-            className="path"
-            src={require("assets/img/path1.png").default}
-          />
-          <Container className="align-items-center">
-            <Row>
-              <Col lg="6" md="6">
-                Example of first thing
-                <br />
-                <Button color="info" onClick={handleClick}>
-                  Info
+          <div className="page-header-image" />
+          <div className="content">
+            <Container>
+              <Row className="mb-4">
+                <Button onClick={getClauses}>Get Clauses</Button>
+              </Row>
+              <Row>
+                {clauseList.map((item, key) => (
+                  <span key={key + "-clause"}>
+                    <p className="text-left" key={key + "-num-Clause"}>
+                      <b>{key + 1}. </b>
+                      {item}
+                    </p>
+                    <Button
+                      className="btn-round"
+                      color="primary"
+                      size="lg"
+                      name={key}
+                      onClick={acceptClause}
+                    >
+                      Accept
+                    </Button>
+                  </span>
+                ))}
+              </Row>
+              <Row>
+                <Button
+                  className="btn-round"
+                  color="primary"
+                  size="lg"
+                  onClick={checkAccepted}
+                >
+                  Check
                 </Button>
-              </Col>
-            </Row>
-            <br />
-            <br />
-            <Row>
-              <Col lg="6" md="6">
-                Example of Second thing
-                <br />
-                <Button color="info">Info</Button>
-              </Col>
-            </Row>
-          </Container>
+              </Row>
+            </Container>
+          </div>
         </div>
         <Footer />
       </div>
