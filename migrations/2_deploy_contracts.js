47b15c7180e60f60d20a28ef37f7ba3000694ca2
Noe Horowitz <noehoro@gmail.com>
2021-07-13 17:14:42 -0400

    Deployment of tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,27 @@
 const Covid19usecase = artifacts.require("./Covid19usecase.sol");
+var MyToken = artifacts.require("./MyToken.sol");
+var MyTokenSales = artifacts.require("./MyTokenSale.sol");
+var KycContract = artifacts.require("./KycContract.sol");
+require("dotenv").config({ path: "../.env" });
 
-module.exports = function (deployer) {
+module.exports = async (deployer) => {
+  // Agreement Contract
   deployer.deploy(Covid19usecase);
+
+  // Tokens
+  let addr = await web3.eth.getAccounts();
+  await deployer.deploy(MyToken, process.env.INITIAL_TOKENS);
+  await deployer.deploy(KycContract);
+  await deployer.deploy(
+    MyTokenSales,
+    1,
+    addr[0],
+    MyToken.address,
+    KycContract.address
+  );
+  let tokenInstance = await MyToken.deployed();
+  await tokenInstance.transfer(
+    MyTokenSales.address,
+    process.env.INITIAL_TOKENS
+  );
 };
