421b9de7a025678320a50264c2ed3fec6af5a265
Noe Horowitz <noehoro@gmail.com>
2021-07-27 15:17:34 -0400

    cleanup login page

--- client/src/views/LoginPage.jsx
+++ client/src/views/LoginPage.jsx
@@ -1,5 +1,4 @@
 import React, { useContext, useState, useEffect, useCallback } from "react";
-import classnames from "classnames";
 
 // reactstrap components
 import {
@@ -12,12 +11,6 @@ import {
   Container,
   Row,
   Col,
-  CardBody,
-  Nav,
-  NavItem,
-  NavLink,
-  TabContent,
-  TabPane,
 } from "reactstrap";
 
 // core components
@@ -25,46 +18,9 @@ import NavBar from "components/NavBar";
 import Footer from "components/Footer";
 import Context from "../Helpers/Context";
 import getWeb3 from "../Helpers/getWeb3";
-import ResearcherRoles from "../contracts/ResearcherRoles.json";
 
 const LoginPage = () => {
   const { contextValue, dispatchContextValue } = useContext(Context);
-  const [roleTab, setRoleTab] = useState(1);
-  const [contracts, setContracts] = useState({
-    researcherRoles: {},
-  });
-  const [role, setRole] = useState("");
-
-  const updateRole = useCallback(
-    async (instance) => {
-      const _role = await instance.methods
-        .getAccountRole(contextValue.web3.accounts[0])
-        .call();
-      setRole(_role);
-    },
-    [contextValue.web3.accounts]
-  );
-  useEffect(() => {
-    const init = async () => {
-      try {
-        const web3 = await getWeb3();
-        const instance = new web3.eth.Contract(
-          ResearcherRoles.abi,
-          ResearcherRoles.networks[contextValue.web3.networkId] &&
-            ResearcherRoles.networks[contextValue.web3.networkId].address
-        );
-
-        updateRole(instance);
-        setContracts((c) => ({
-          ...c,
-          researcherRoles: instance,
-        }));
-      } catch (error) {
-        console.log("Error");
-      }
-    };
-    if (contextValue.loggedIn) init();
-  }, [contextValue.loggedIn, contextValue.web3.networkId, updateRole]);
 
   const handleLogin = async () => {
     try {
@@ -93,13 +49,6 @@ const LoginPage = () => {
     dispatchContextValue({
       type: "logout",
     });
-  };
-
-  const handleRoleChange = async () => {
-    await contracts.researcherRoles.methods
-      .changeAccountRole(roleTab)
-      .send({ from: contextValue.web3.accounts[0] });
-    await updateRole(contracts.researcherRoles);
   };
 
   return (
@@ -155,79 +104,6 @@ const LoginPage = () => {
                     </CardFooter>
                   </Card>
                 </Col>
-                {contextValue.loggedIn ? (
-                  <Col className="offset-lg-0 offset-md-3" xs="12" md="6">
-                    <Card className="card-register p-4">
-                      <CardHeader>
-                        <Nav className="nav-tabs-info" role="tablist" tabs>
-                          <NavItem>
-                            <NavLink
-                              className={classnames({
-                                active: roleTab === 1,
-                              })}
-                              onClick={(e) => setRoleTab(1)}
-                              href="#"
-                            >
-                              <i className="tim-icons icon-spaceship" />
-                              First Role
-                            </NavLink>
-                          </NavItem>
-                          <NavItem>
-                            <NavLink
-                              className={classnames({
-                                active: roleTab === 2,
-                              })}
-                              onClick={(e) => setRoleTab(2)}
-                              href="#"
-                            >
-                              <i className="tim-icons icon-settings-gear-63" />
-                              Second Role
-                            </NavLink>
-                          </NavItem>
-                          <NavItem>
-                            <NavLink
-                              className={classnames({
-                                active: roleTab === 3,
-                              })}
-                              onClick={(e) => setRoleTab(3)}
-                              href="#"
-                            >
-                              <i className="tim-icons icon-bag-16" />
-                              Third Role
-                            </NavLink>
-                          </NavItem>
-                        </Nav>
-                      </CardHeader>
-                      <CardBody>
-                        <TabContent
-                          className="tab-space"
-                          activeTab={"link" + roleTab}
-                        >
-                          <TabPane tabId="link1">
-                            <p>This is what the first role does.</p>
-                          </TabPane>
-                          <TabPane tabId="link2">
-                            <p>This is what the second role does</p>
-                          </TabPane>
-                          <TabPane tabId="link3">
-                            <p>This is what the third role does</p>
-                          </TabPane>
-                        </TabContent>
-                        <Button
-                          className="btn-round"
-                          color="info"
-                          size="lg"
-                          onClick={handleRoleChange}
-                        >
-                          Change Role
-                        </Button>
-                      </CardBody>
-                      <CardFooter>Your role is {role}</CardFooter>
-                    </Card>
-                  </Col>
-                ) : (
-                  <></>
-                )}
               </Row>
             </Container>
           </div>
