bf0f3a3d00006f36bc1ce225aa0a49edafeaa7aa
Noe Horowitz <noehoro@gmail.com>
2021-07-27 15:14:50 -0400

    Roles Smart contract v1

--- contracts/Roles.sol
+++ contracts/Roles.sol
@@ -1,39 +1,52 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.6.0;
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./Libraries/AccessControl.sol";
 
 /// @dev Implements a single role access control contract.
-contract Community is AccessControl {
-    string private roleName;
+contract Roles is AccessControl {
+    string[2] RoleNames = ["TEST_ADMIN_ROLE", "TEST_ROLE"];
+    bytes32[2] RolesList = [
+        keccak256("TEST_ADMIN_ROLE"),
+        keccak256("TEST_ROLE")
+    ];
 
     /// @dev Create the community role, with `root` as a member.
-    constructor(address root, string memory _roleName) public {
-        _setupRole(DEFAULT_ADMIN_ROLE, root);
-        roleName = _roleName;
+    constructor(address test_admin_role_root, address test_role_root) public {
+        _setupRole(RolesList[0], test_admin_role_root, DEFAULT_ADMIN_ROLE);
+        _setupRole(RolesList[1], test_role_root, RolesList[0]);
     }
 
-    /// @dev Restricted to members of the community.
-    modifier onlyMember() {
-        require(isMember(msg.sender), "Restricted to members.");
-        _;
+    function getRolesList() public view returns (bytes32[2] memory) {
+        return (RolesList);
     }
 
-    /// @dev Return `true` if the `account` belongs to the community.
-    function isMember(address account) public view virtual returns (bool) {
-        return hasRole(DEFAULT_ADMIN_ROLE, account);
+    function requestJoin(bytes32 role) public {
+        _requestJoin(role);
     }
 
-    /// @dev Add a member of the community.
-    function addMember(address account) public virtual onlyMember {
-        grantRole(DEFAULT_ADMIN_ROLE, account);
+    function getRequests(bytes32 role) public view returns (address[] memory) {
+        return (_getRequests(role));
     }
 
-    /// @dev Remove oneself as a member of the community.
-    function leaveCommunity() public virtual {
-        renounceRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    function acceptRequest(bytes32 role, uint256 index) public onlyAdmin(role) {
+        _acceptRequest(role, index);
     }
 
-    function returnRoleName() public view returns (string memory) {
-        return roleName;
+    function testFunction()
+        public
+        view
+        onlyMember(RolesList[1])
+        returns (bool)
+    {
+        return true;
+    }
+
+    function testAdminFunction()
+        public
+        view
+        onlyAdmin(RolesList[1])
+        returns (bool)
+    {
+        return true;
     }
 }
