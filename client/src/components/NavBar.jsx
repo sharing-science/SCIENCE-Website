921d6f99f7d05ae973007d1986bd199f3f18e9e3
Yuerden <john.cohen.work@gmail.com>
2023-02-21 15:48:12 -0500

    Removed Hash from dropdown

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -207,10 +207,10 @@ const NavBar = () => {
                   <i className="tim-icons icon-cloud-upload-94" />
                   Check Access
                 </DropdownItem>
-                <DropdownItem tag={Link} to="/hash">
+                {/* <DropdownItem tag={Link} to="/hash">
                   <i className="tim-icons icon-cloud-upload-94" />
                   Hash
-                </DropdownItem>
+                </DropdownItem> */}
               </DropdownMenu>
             </UncontrolledDropdown>
             <NavItem>
