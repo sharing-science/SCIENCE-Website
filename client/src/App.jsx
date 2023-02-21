d825cf283a83f3ebfcdf3930298bda2ddebfa856
Yuerden <john.cohen.work@gmail.com>
2023-02-21 15:08:21 -0500

    Removed HashTestPage
    Hash is complete and no longer required to test.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,7 +21,7 @@ import { contextReducer } from 'Helpers/reducers'
 import { getLocalStorageObject } from 'Helpers/helperFunctions'
 import { PrivateRoute, PublicRoute } from './Helpers/Routes'
 import CheckAccessPage from 'views/CheckAccessPage'
-import HashTestPage from 'views/HashTestPage'
+// import HashTestPage from 'views/HashTestPage'  //no longer needed
 
 const App = () => {
   const [contextValue, dispatchContextValue] = useReducer(contextReducer, {
@@ -45,7 +45,7 @@ const App = () => {
           <PrivateRoute exact path="/team" component={TeamPage} />
           <PrivateRoute exact path="/newFile" component={CreateNewFilePage} />
           <PrivateRoute exact path="/check" component={CheckAccessPage} />
-          <PrivateRoute exact path="/hash" component={HashTestPage} />
+          {/* <PrivateRoute exact path="/hash" component={HashTestPage} /> */}
           <PrivateRoute exact path="/approveRequests" component={ApproveRequestsPage} />
 
           {/* Public Routes */}
